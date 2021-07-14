4e4617d2e239f9c26e12a6dc204289a3ca467148
Odilboyev <samandarodilboyev@gmail.com>
2021-07-14 17:56:09 +0500

    added delete button

--- src/Containers/Todo/Todo.js
+++ src/Containers/Todo/Todo.js
@@ -1,54 +1,76 @@
 import { AiOutlinePlus } from 'react-icons/ai';
+import { BsTrash } from 'react-icons/bs';
 import React, { Component } from 'react'
-import { Button, Input,} from 'reactstrap';
+import { Button, Input, } from 'reactstrap';
 import { ListGroup, ListGroupItem } from 'reactstrap';
 import './Todo.css';
 
 
 export default class Todo extends Component {
-    constructor(props){
-      super(props);
-      
-      this.state = {todos: this.props.todos, typing: 'task'}
-    }
+  constructor(props) {
+    super(props);
+    this.state = { todos: this.props.todos, typing: 'Type your task' }
+  }
 
-    setValues = (e) => {
-      this.setState({typing: e.target.value})
-    }
+  setValues = (e) => {
+    this.setState({ typing: e.target.value })
+  }
 
-    setTodo = () => {
+  setTodo = () => {
+    if (this.state.typing !== "") {
       let data = [...this.state.todos]
-      data.push({title:this.state.typing})
-      this.setState({todos: data})
+      data.push({ title: this.state.typing })
+      this.setState({ todos: data })
+      this.setState({ typing: "" })
+    } else {
+      alert('Type your task !')
     }
-    render() {
-        return (
-            <div className="rounded overflow-hidden shadow p-3 bg-light">
-              <h2>Todo App</h2>
+  }
 
-              <div className="d-flex align-items-center mb-3">
-              <Input 
-              type="text" 
-              placeholder="New Task" 
-              value ={this.state.typing} 
-              className="me-2"
-              onChange={this.setValues}
-              />
-              <Button 
-              color="primary"
-              onClick={this.setTodo} 
-              >
-                <AiOutlinePlus color="white" fontSize="1.5em" />
-              </Button>
-              </div>
-              <ListGroup>
-                {
-                    this.state.todos.map((item, index)=>{
-                       return  <ListGroupItem tag="a" href="#" key={index} action>{index + 1}.  {item.title}</ListGroupItem>
-                    })
-                }
-              </ListGroup>
-            </div>
-        )
-    }
+  deleteTodo = (index) => {
+    let data = [...this.state.todos]
+    data.splice(index, 1)
+    this.setState({ todos: data })
+    this.setState({ typing: "" })
+  }
+
+  render() {
+    return (
+      <div className="rounded overflow-hidden shadow p-3 bg-light">
+        <h2>Todo App</h2>
+
+        <div className="d-flex align-items-center mb-3">
+          <Input
+            type="text"
+            placeholder="New Task"
+            value={this.state.typing}
+            className="me-2"
+            onChange={this.setValues}
+          />
+          <Button
+            color="primary"
+            onClick={this.setTodo}
+          >
+            <AiOutlinePlus color="white" fontWeight="600" fontSize="1.5rem" />
+          </Button>
+        </div>
+
+        <ListGroup>
+          {
+            this.state.todos.map((item, index) => {
+              return <ListGroupItem tag="a" href="#" className="w-100" key={index} action>{index + 1}.  {item.title}
+                <Button
+                  color="danger"
+                  className="float-right"
+                  onClick={this.deleteTodo.bind(this, index)}
+                >
+                  <BsTrash color="white" fontWeight="600" fontSize="1.5rem" />
+                </Button>
+              </ListGroupItem>
+            })
+          }
+        </ListGroup>
+      </div>
+    )
+  }
 }
